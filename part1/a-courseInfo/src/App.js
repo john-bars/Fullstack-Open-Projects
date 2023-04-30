a42489b911da7afc5de14b887daf9cc3a29cc654
John Bars <john.barsq@gmail.com>
2023-04-30 20:56:01 +0800

    use map and forEach loop to get the total sum of the exercises

--- part1/a-courseInfo/src/App.js
+++ part1/a-courseInfo/src/App.js
@@ -3,13 +3,25 @@ const Header = (props) => {
 };
 
 const Content = (props) => {
+  // console.log(props);
   const content = props.parts.map((part) => (
     <p>
       {" "}
-      {part.name}: {part.exercises}{" "}
+      {part.name} {part.exercises}{" "}
     </p>
   ));
   return content;
+};
+
+const Total = (props) => {
+  let sum = 0;
+  const exercisesArray = props.parts.map((part) => part.exercises);
+  // console.log(exercisesArray);
+  exercisesArray.forEach((num) => {
+    sum += num;
+  });
+  // console.log(sum);
+  return <p>Number of exercises: {sum}</p>;
 };
 
 const App = () => {
@@ -26,7 +38,7 @@ const App = () => {
     <div>
       <Header course={course} />
       <Content parts={course.parts} />
-      {/* <Total total={course.parts} /> */}
+      <Total parts={course.parts} />
     </div>
   );
 };
