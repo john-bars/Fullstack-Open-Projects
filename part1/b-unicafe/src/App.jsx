dbe92184bba024e33c6973b55651445364ab0007
John Bars <john.barsq@gmail.com>
2023-05-01 15:10:52 +0800

    unicafe step5 update: refactoring

--- part1/b-unicafe/src/App.jsx
+++ part1/b-unicafe/src/App.jsx
@@ -4,6 +4,14 @@ const Button = ({ handleClick, text }) => {
   return <button onClick={handleClick}>{text}</button>;
 };
 
+const StatisticLine = ({ text, value }) => {
+  return (
+    <p>
+      {text} {value}
+    </p>
+  );
+};
+
 const Statistics = ({ good, neutral, bad }) => {
   const total = good + bad + neutral;
   const average = (good - bad) / total;
@@ -19,15 +27,16 @@ const Statistics = ({ good, neutral, bad }) => {
   return (
     <div>
       <h1>Statistics</h1>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {total}</p>
-      <p>average {average}</p>
-      <p>positive {positive}</p>
+      <StatisticLine text="good" value={good} />
+      <StatisticLine text="neutral" value={neutral} />
+      <StatisticLine text="bad" value={bad} />
+      <StatisticLine text="all" value={total} />
+      <StatisticLine text="average" value={average} />
+      <StatisticLine text="positive" value={positive} />
     </div>
   );
 };
+
 const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
@@ -36,35 +45,9 @@ const App = () => {
   return (
     <div>
       <h1>give feedback</h1>
-      <Button
-        handleClick={() => {
-          // const newGoodValue = good + 1;
-          // console.log(
-          //   "good value now: ",
-          //   newGoodValue,
-          //   "previous value is: ",
-          //   good
-          // );
-          setGood(good + 1);
-        }}
-        text="good"
-      />
-      <Button
-        handleClick={() => {
-          // const newNeutralValue = neutral + 1;
-          // console.log("neutral value now: ", newNeutralValue);
-          setNeutral(neutral + 1);
-        }}
-        text="neutral"
-      />
-      <Button
-        handleClick={() => {
-          // const newBadValue = bad + 1;
-          // console.log("bad value now: ", newBadValue);
-          setBad(bad + 1);
-        }}
-        text="bad"
-      />
+      <Button handleClick={() => setGood(good + 1)} text="good" />
+      <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
+      <Button handleClick={() => setBad(bad + 1)} text="bad" />
       <Statistics good={good} bad={bad} neutral={neutral} />
     </div>
   );
