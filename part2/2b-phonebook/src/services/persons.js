9eec9f119e09b7c9a2a6c317d986ae1096456969
John Bars <john.barsq@gmail.com>
2023-05-06 14:32:28 +0800

    create a services module to handle the communication with the backend.

---
+++ part2/2b-phonebook/src/services/persons.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+const baseUrl = "http://localhost:3001/persons";
+
+const getAll = () => {
+  return axios.get(baseUrl).then((res) => res.data);
+};
+
+const create = (newObject) => {
+  return axios.post(baseUrl, newObject).then((res) => res.data);
+};
+
+const update = (id, newObject) => {
+  return axios.put(`${baseUrl}/${id}`, newObject).then((res) => res.data);
+};
+
+const phoneBookService = { getAll, create, update };
+export default phoneBookService;
