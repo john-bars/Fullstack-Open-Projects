3a8bd78b3d6b8e9403b2a9862fe6cb7f3efdff42
John Bars <john.barsq@gmail.com>
2023-05-07 15:47:47 +0800

    create delete button and handleDelete method

--- part2/2b-phonebook/src/components/Persons.js
+++ part2/2b-phonebook/src/components/Persons.js
@@ -1,19 +1,30 @@
-import React from "react";
+import phoneBookService from "../services/persons";
 
-export default function Persons({ filterInput, filteredNames, persons }) {
+const Persons = ({ filterInput, filteredNames, persons, setPersons }) => {
+  const handleDelete = (person) => {
+    console.log("person: ", person);
+    phoneBookService
+      .remove(person.id)
+      .then(() => phoneBookService.getAll().then((res) => setPersons(res)));
+  };
+
   return (
     <div>
       {filterInput === ""
         ? persons.map((person) => (
             <p key={person.id}>
-              {person.name} {person.number} {person.id}
+              {person.name} {person.number} {person.id}{" "}
+              <button onClick={() => handleDelete(person)}>Delete</button>
             </p>
           ))
         : filteredNames.map((person) => (
             <p key={person.id}>
-              {person.name} {person.number} {person.id}
+              {person.name} {person.number} {person.id}{" "}
+              <button onClick={() => handleDelete(person)}>Delete</button>
             </p>
           ))}
     </div>
   );
-}
+};
+
+export default Persons;
