8174a808bdb0155bfd3d210250d9b76b4222be95
John Bars <john.barsq@gmail.com>
2023-05-09 15:34:00 +0800

    phonebook step12: modify success and error message.

--- part2/2b-phonebook/src/components/Notification.js
+++ part2/2b-phonebook/src/components/Notification.js
@@ -3,8 +3,14 @@ import "../index.css";
 const Notification = ({ message }) => {
   if (message === null) {
     return null;
+  } else {
+    return (
+      <div>
+        {message.success && <div className="success">{message.success}</div>}
+        {message.error && <div className="error">{message.error}</div>}
+      </div>
+    );
   }
-  return <div className="success">{message}</div>;
 };
 
 export default Notification;
