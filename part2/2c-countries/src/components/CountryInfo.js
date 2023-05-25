75bd469912e5beda24d38a0216f7ebde9399e11c
John Bars Quipia <john.barsq@gmail.com>
2023-05-25 13:47:56 +0800

    data for country step2 update: create components to show the list of filtered countries and their info once the button is clicked

--- part2/2c-countries/src/components/CountryInfo.js
+++ part2/2c-countries/src/components/CountryInfo.js
@@ -1,35 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
+
+const CountryList = ({ filteredCountries, show, setShow }) => {
+  return filteredCountries.map((country) => (
+    <p key={country.cca2}>
+      {country.name.common}
+      <button onClick={() => setShow(!show)}>show</button>
+    </p>
+  ));
+};
+
+const Info = ({ filteredCountries }) => {
+  return filteredCountries.map((country) => (
+    <div key={country.cca2}>
+      <h2>{country.name.common}</h2>
+      {/* capital {country.capital} <br /> */}
+      area {country.area} <br />
+      <p>
+        <strong>languages: </strong>
+      </p>
+      <ul>
+        {Object.keys(country.languages).map((lang) => (
+          <li key={lang}>{country.languages[lang]}</li>
+        ))}
+      </ul>
+      <img alt={country.flags.alt} src={country.flags.png} />
+      <h2>Weather in {country.capital}</h2>
+      temperature
+    </div>
+  ));
+};
 
 export default function CountryInfo({ filteredCountries }) {
+  const [show, setShow] = useState(false);
   if (!filteredCountries) {
     return;
   }
 
   return filteredCountries.length > 10 ? (
     <p>too many match, specify another filter</p>
-  ) : filteredCountries.length > 1 ? (
-    filteredCountries.map((country) => (
-      <p key={country.cca2}>
-        {country.name.common}
-        <button onClick={() => console.log("clicked")}>show</button>
-      </p>
-    ))
+  ) : filteredCountries.length < 1 ? (
+    <Info filteredCountries={filteredCountries} />
+  ) : show ? (
+    <Info filteredCountries={filteredCountries} />
   ) : (
-    filteredCountries.map((country) => (
-      <div key={country.cca2}>
-        <h2>{country.name.common}</h2>
-        capital {country.capital[0]} <br />
-        area {country.area} <br />
-        <p>
-          <strong>languages: </strong>
-        </p>
-        <ul>
-          {Object.keys(country.languages).map((lang) => (
-            <li key={lang}>{country.languages[lang]}</li>
-          ))}
-        </ul>
-        <img alt={country.flags.alt} src={country.flags.png} />
-      </div>
-    ))
+    <CountryList
+      filteredCountries={filteredCountries}
+      show={show}
+      setShow={setShow}
+    />
   );
 }
