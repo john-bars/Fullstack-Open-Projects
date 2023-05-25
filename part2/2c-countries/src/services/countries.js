20c3606a1ede2cc60ed3b209fd0f65b09da5c50e
John Bars Quipia <john.barsq@gmail.com>
2023-05-25 13:42:29 +0800

    create an axios get request for individual country.

--- part2/2c-countries/src/services/countries.js
+++ part2/2c-countries/src/services/countries.js
@@ -5,21 +5,9 @@ const getAll = () => {
   return axios.get(`${baseUrl}/all`).then((res) => res.data);
 };
 
-// const getName = () => {
-//   return axios.get(`${baseUrl}/name`)
-// }
-
-const create = (newObject) => {
-  return axios.post(baseUrl, newObject).then((res) => res.data);
+const getName = (name) => {
+  return axios.get(`${baseUrl}/name/${name}`).then((res) => res.data);
 };
 
-const update = (id, newObject) => {
-  return axios.put(`${baseUrl}/${id}`, newObject).then((res) => res.data);
-};
-
-const remove = (id) => {
-  return axios.delete(`${baseUrl}/${id}`);
-};
-
-const countriesAPI = { getAll, create, update, remove };
+const countriesAPI = { getAll, getName };
 export default countriesAPI;
